e4752f368fdabc4cd3bbfa1cf74fc273ec700625
NaianeG <naianegoulartoliveira@gmail.com>
2024-11-08 12:06:49 -0300

    Listas de produtos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    const url = "http://localhost:3000/products";
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+
+    const { data: items, httpConfig, loading, error } = useFetch(url);
+
+    useEffect(() => {
+        if (items) setProducts(items);
+    }, [items]);
+
+    const handlleSubmit = async (e) => {
+        e.preventDefault();
+        const product = { name, price };
+        httpConfig(product, "POST");
+        setName("");
+        setPrice("");
+    };
+
+    const handleRemove = (id) => {
+        httpConfig(id, "DELETE");
+    };
+
+    return (
+        <div className="App">
+            <h1>Lista de Produtos</h1>
+            {loading && <p>Carregando dados...</p>}
+            {error && <p>{error}</p>}
+            {!loading && (
+                <ul>
+                    {products.map((product) => (
+                        <li key={product.id}>
+                            {product.name} - R$: {product.price}
+                            <button onClick={() => handleRemove(product.id)}>Excluir</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+            <div className='add-product'>
+                <form onSubmit={handlleSubmit}>
+                    <label>
+                        Nome:
+                        <input
+                            type="text"
+                            value={name}
+                            name="name"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Preço:
+                        <input
+                            type="text"
+                            value={price}
+                            name="price"
+                            onChange={(e) => setPrice(e.target.value)}
+                        />
+                    </label>
+                    {loading ? <input type="submit" disabled value="Aguarde" /> : <input type="submit" value="Criar" />}
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
